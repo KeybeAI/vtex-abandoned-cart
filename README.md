c6d70db0d817d06c105b593673960ba5380a8275
TodoViernes <santiagovallejoc@gmail.com>
2023-01-25 08:48:06 -0600

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Keybe Abandoned Carts VTEX Script
+
+### How to implement
+
+Include the script on the checkout scripts and replace YOUR_COMPANY_UUID, YOUR_APP_UUID, YOUR_PUBLIC_KEY for your keys.
+
+The keys are located in [Keybe settings](https://keybe.app/admin/configurations/app "settings").
+
+Before you insall the script you need to generate a webhook to control the flow.
+
+To create a webhook in VTEX you need to generate the keys under the VTEX store admin.
+App keys are located on: 👇
+https://yourstoreslug.myvtex.com/admin/appkeys
+
+### Example
+
+```Language
+curl --location --request POST 'https://yourstoreslug.myvtex.com/api/orders/hook/config' \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json' \
+--header 'X-VTEX-API-AppKey: YOUR_APP_KEY' \
+--header 'X-VTEX-API-AppToken: YOUR_APP_TOKEN' \
+--data-raw '{
+  "filter": {
+      "type": "FromWorkflow",
+      "status": ["order-created"]
+  },
+  "hook": {
+     "url": "https://nwp3x7ssva.execute-api.us-east-1.amazonaws.com/prod/vtex/sell-confirm",
+  }
+}'
+```
+### NOTE:
+You need to change YOUR_APP_KEY and YOUR_APP_TOKEN for the ones you generated on your store.
+
+The webhook will guarantee the integration flow behavior.
---
+++ keybe-abandoned-cart.js
@@ -0,0 +1,65 @@
+//Keybe AI Integration
+//This script is used to integrate Keybe AI with VTEX
+//We added this log to check if the script is loaded
+console.log('keybe-checkout-ready')
+
+
+window.onload = event => {
+  setInterval(() => {
+    if (window?.vtexjs?.checkout?.orderForm?.orderFormId !== '' && window.vtexjs.checkout.orderForm.orderFormId !== undefined) {
+      fetch('https://nwp3x7ssva.execute-api.us-east-1.amazonaws.com/prod/vtex/sync-contact', {
+        method: 'POST',
+        body: JSON.stringify({
+          orderId: window?.vtexjs?.checkout?.orderForm?.orderFormId,
+          dataClient: {
+            companyUUID: 'YOUR_COMPANY_UUID',
+            appUUID: 'YOUR_APP_UUID',
+            publicKey: 'YOUR_PUBLIC_KEY',
+            haveSearchPhoneUser: true
+          },
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+  }, 10000);
+
+
+  const inputPhone = window.document.getElementById('client-phone')
+  inputPhone.addEventListener('blur', async () => {
+    await fetch('https://nwp3x7ssva.execute-api.us-east-1.amazonaws.com/prod/vtex/sync-user-by-data', {
+      method: 'POST',
+      body: JSON.stringify({
+        userData: {
+          name:
+            window?.vtexjs?.checkout?.orderForm?.clientProfileData
+              ?.firstName,
+          lastName:
+            window?.vtexjs?.checkout?.orderForm?.clientProfileData
+              ?.lastName,
+          email:
+            window?.vtexjs?.checkout?.orderForm?.clientProfileData?.email,
+          phone:
+            window?.vtexjs?.checkout?.orderForm?.clientProfileData?.phone,
+          countryCode: '57',
+          extradata: [
+            {
+              field: 'abandono el carrito',
+              value: 'si',
+            },
+          ],
+        },
+        keybeClientData: {
+          companyUUID: 'YOUR_COMPANY_UUID',
+          appUUID: 'YOUR_APP_UUID',
+          publicKey: 'YOUR_PUBLIC_KEY',
+          haveSearchPhoneUser: true
+        }
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  })
+}
